b7bbd389a886c9b4dbb7f5c73d7a08f15e2385a8
Nicholas Warner <nswarner2@gmail.com>
2021-04-10 17:35:00 -0400

    Created ACM Certificate module.

---
+++ modules/acm_certificate/main.tf
@@ -0,0 +1,10 @@
+resource "aws_acm_certificate" "acm_certificate" {
+
+  domain_name               = var.domain_name
+  subject_alternative_names = var.subject_alternative_names
+  validation_method         = var.validation_method
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ modules/acm_certificate/variables.tf
@@ -0,0 +1,12 @@
+variable "domain_name" {
+  description = "Domain name for the AWS Certificate Manager request."
+}
+
+variable "subject_alternative_names" {
+  description = "Subject Alternative Names (SAN) for the AWS Certificate Manager request."
+}
+
+variable "validation_method" {
+  description = "Method for validating the AWS Certificate Manager request."
+  default     = "DNS"
+}
