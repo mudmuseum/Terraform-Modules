8dad2b73a8b1ccf96e2b568f6977aa3a9dff7cd7
Nicholas Warner <nswarner2@gmail.com>
2021-04-10 17:22:54 -0400

    Deleting VPC Base Infra mono module.

--- modules/vpc_base_infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "VPC ID."
-  value       = aws_vpc.vpc.id
-}
-
-output "public_subnet_id" {
-  description = "Public Subnet ID."
-  value       = aws_subnet.public_subnet.id
-}
