6aafcabaa6bf8e4548fb76d81f9bc5dc95fdb344
Nicholas Warner <nswarner2@gmail.com>
2021-04-11 13:03:58 -0400

    Added path to the Origin Access Identity outputs.

--- modules/cloudfront_origin_access_identity/outputs.tf
+++ modules/cloudfront_origin_access_identity/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "The Origin Access Identity ID value."
   value = aws_cloudfront_origin_access_identity.origin_access_identity.id
 }
+
+output "cloudfront_access_identity_path" {
+  description = "The Origin Access Identity Path value."
+  value = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+}
