92b91a4cc147bf5aed6d6a5bde7d20c625e3fe86
Nicholas Warner <nswarner2@gmail.com>
2021-04-11 14:05:21 -0400

    Added support for lifecycle_rule block.

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -6,6 +6,21 @@ resource "aws_s3_bucket" "s3_bucket" {
     target_prefix = var.logging_target_prefix
   }
 
+  lifecycle_rule {
+    id                                     = var.lifecycle_id
+    enabled                                = var.enabled
+    abort_incomplete_multipart_upload_days = var.abort_incomplete_multipart
+
+    expiration {
+      days                         = var.expiration_days
+      expired_object_delete_marker = var.expired_object_delete_marker
+    }
+
+    noncurrent_version_expiration {
+      days = var.noncurrent_days
+    }
+  }
+
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -19,3 +19,33 @@ variable "force_destroy" {
   description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
   default     = false
 }
+
+variable "lifecycle_id" {
+  description = "The ID for the lifecycle rule."
+  default     = ""
+}
+
+variable "enabled" {
+  description = "Whether the lifecycle rule is enabled or not."
+  default     = null
+}
+
+variable "abort_incomplete_multipart" {
+  description = "Abort Incomplete Multipart Upload after value days."
+  default     = ""
+}
+
+variable "expiration_days" {
+  description = "Objects expire after value days."
+  default     = ""
+}
+
+variable "expired_object_delete_marker" {
+  description = "Determines if there's an expired object delete marker."
+  default     = null
+}
+
+variable "noncurrent_days" {
+  description = "Expiration time in days for noncurrent versions."
+  default     = ""
+}
