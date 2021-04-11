74beb9b228047a113a9ecf00218ed427317f0105
Nicholas Warner <nswarner2@gmail.com>
2021-04-11 12:59:26 -0400

    Updated outputs.

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -2,3 +2,8 @@ output "name" {
   description = "Bucket name"
   value       = aws_s3_bucket.s3_bucket.id
 }
+
+output "bucket_regional_domain_name" {
+  description = "Bucket Regional Domain name."
+  value       = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
+}
