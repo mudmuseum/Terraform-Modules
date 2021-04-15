a7158db14212d04b1288ea3442f9f7084c09acb4
Nicholas Warner <nswarner2@gmail.com>
2021-04-15 14:21:24 -0400

    Added support for tags on Route53 Zone.

--- modules/route53_zone/main.tf
+++ modules/route53_zone/main.tf
@@ -1,5 +1,7 @@
 resource "aws_route53_zone" "route53_zone" {
-  name          = var.route53_zone_name
+  name = var.route53_zone_name
+
+  tags = var.tags
 
   lifecycle {
     prevent_destroy = true
--- modules/route53_zone/variables.tf
+++ modules/route53_zone/variables.tf
@@ -1,3 +1,9 @@
 variable "route53_zone_name" {
   description = "The Route53 Zone name."
 }
+
+variable "tags" {
+  description = "Tags for the Route53 Zone."
+  type        = map
+  default     = { }
+}
