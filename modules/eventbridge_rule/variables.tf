c6ec601259c1b74555852fa26b96ac3b7cb8a3a9
Nicholas Warner <nswarner2@gmail.com>
2021-04-16 14:16:08 -0400

    Fixing optional variable.

--- modules/eventbridge_rule/variables.tf
+++ modules/eventbridge_rule/variables.tf
@@ -10,9 +10,9 @@ variable "pattern" {
   description = "The Pattern to match for the EventBridge Rule."
 }
 
-variable "role_arn" {
- description = "The Role ARN to associate with the EventBridge Rule."
-}
+# variable "role_arn" {
+#  description = "The Role ARN to associate with the EventBridge Rule."
+# }
 
 variable "tags" {
   description = "The Tags to associate with the EventBridge Rule."
