2b14f52bdd52516e54813958f56469f926f43aef
Nicholas Warner <nswarner2@gmail.com>
2021-04-16 14:35:51 -0400

    Added Role ARN to IAM Role output.

--- modules/iam_role/outputs.tf
+++ modules/iam_role/outputs.tf
@@ -2,3 +2,8 @@ output "role_name" {
   description = "The AWS IAM Role name."
   value       = aws_iam_role.iam_role.name
 }
+
+output "role_arn" {
+  description = "The AWS IAM Role ARN."
+  value       = aws_iam_role.iam_role.arn
+}
